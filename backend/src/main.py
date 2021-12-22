55bc2a12b888f0eaf9198c0d573614a30900c4a3
Орлов Ярослав <y.orlov@fonmix.ru>
2021-12-23 01:49:27 +0300

    'main[m: add vue hello world from backend'

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex='.*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get('/')
 def root():
-    return 'hello world'
+    return 'Привет мир!!!'
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios'
 import Vue from 'vue'
+
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+axios.defaults.withCredentials = true;
+
+// (1)
+axios.defaults.baseURL = 'http://localhost:5000/';
+Vue.config.productionTip = false;
 
 new Vue({
   router,
